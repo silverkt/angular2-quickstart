ca4378821b21fe8317306a249c9b2224b4237a85
Jean-Xu <zms_jean@163.com>
2016-10-27 14:12:48 +0800

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -20,9 +20,6 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.ts']
     },
-    // webpack-dev-server 只会在内存中打包静态资源 用于开发环境
-    // 生成环境需要 webpack 打包
-    // 如果无法热替换 应该是电脑的原因 请重启电脑，有时使用webstrom里也无法更新，真实项目可以换成 browser-sync
     devServer:{
         historyApiFallback: true,
         hot: true,
